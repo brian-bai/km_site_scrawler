7934df9457842f99879d7a5a7fcc1a6434d6b5d9
Brian Bai <baitq@outlook.com>
2022-01-22 21:59:55 +0800

    format code

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-use std::collections::HashSet;
 use select::document::Document;
 use select::predicate::Name;
-use url::{Url, ParseError};
+use std::collections::HashSet;
+use url::{ParseError, Url};
 //use tempfile::Builder;
 
 use error_chain::error_chain;
@@ -14,58 +14,54 @@ error_chain! {
 }
 
 pub async fn retrieve_urls(home_url: &str) -> Result<HashSet<String>> {
+    //     let res = reqwest::get(home_url)
+    //     .await?
+    //     .text()
+    //     .await?;
 
-//     let res = reqwest::get(home_url)
-//     .await?
-//     .text()
-//     .await?;
-
-//   Document::from(res.as_str())
-//     .find(Name("a"))
-//     .filter_map(|n| n.attr("href"))
-//     .for_each(|x| println!("{}", x));
+    //   Document::from(res.as_str())
+    //     .find(Name("a"))
+    //     .filter_map(|n| n.attr("href"))
+    //     .for_each(|x| println!("{}", x));
 
     let res = reqwest::get(home_url).await?;
     println!("Status for {}: {}", home_url, res.status());
 
-    let body  = res.text().await?;
+    let body = res.text().await?;
     let mut set = HashSet::new();
-       
+
     Document::from(body.as_str())
         .find(Name("a"))
         .filter_map(|n| n.attr("href"))
-        .for_each(|x| { set.insert(String::from(x)); } );
+        .for_each(|x| {
+            set.insert(String::from(x));
+        });
 
     Ok(set)
 }
 
 pub fn compose_absolute_urls(home_url: &str, urls: Vec<String>) -> HashSet<String> {
-
     let base_url = match Url::parse(home_url) {
         Ok(url) => url,
-        Err(why) => panic!("Cannot pare base url: {why}")
+        Err(why) => panic!("Cannot pare base url: {why}"),
     };
     println!("Base Url: {base_url}");
     let mut set = HashSet::new();
     for url in &urls {
         match Url::parse(url) {
             Ok(aurl) => println!("{aurl}"),
-            Err(why) => {
-                
-                match why {
-                    ParseError::RelativeUrlWithoutBase => {
-                        match base_url.join(url) {
-                            Ok(new_url) => {set.insert(new_url.to_string());},
-                            Err(e) => println!("Error {e}"),
-                        }
-                        
+            Err(why) => match why {
+                ParseError::RelativeUrlWithoutBase => match base_url.join(url) {
+                    Ok(new_url) => {
+                        set.insert(new_url.to_string());
                     }
-                    _ => println!("Other error"),
-                }
-            }
+                    Err(e) => println!("Error {e}"),
+                },
+                _ => println!("Other error"),
+            },
         }
-    }    
-    set 
+    }
+    set
 }
 
 pub async fn download(absolute_url: &str, target_dir: &str) -> Result<()> {
@@ -80,23 +76,20 @@ pub async fn download(absolute_url: &str, target_dir: &str) -> Result<()> {
             .unwrap_or("tmp.bin");
 
         println!("file to download: '{}'", fname);
-       
+
         let path = std::path::Path::new(target_dir);
         let fname = path.join(fname);
         println!("will be located under: '{:?}'", fname);
         std::fs::File::create(fname)?
     };
 
-    let mut content =  std::io::Cursor::new(response.bytes().await?);
+    let mut content = std::io::Cursor::new(response.bytes().await?);
     std::io::copy(&mut content, &mut dest)?;
     Ok(())
 }
 
 pub fn extract_extension(url: &str) -> Option<String> {
     let re = regex::Regex::new(r"(?x)\w+\.(?P<ext>\w+)$").unwrap();
-    re.captures(url).and_then(|cap| {
-        cap.name("ext").map(|ext| String::from(ext.as_str()))
-    })
+    re.captures(url)
+        .and_then(|cap| cap.name("ext").map(|ext| String::from(ext.as_str())))
 }
-
-
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use clap::App;
 use clap::Arg;
-use km_site_crawler::retrieve_urls;
 use km_site_crawler::compose_absolute_urls;
 use km_site_crawler::download;
 use km_site_crawler::extract_extension;
+use km_site_crawler::retrieve_urls;
 
 #[macro_export]
 macro_rules! crate_version {
@@ -23,80 +23,77 @@ async fn main() {
                 .short('e')
                 .long("explore")
                 .takes_value(false)
-                .help("View the urls and show the file extensions list.") 
+                .help("View the urls and show the file extensions list."),
         )
         .arg(
             Arg::new("depth")
                 .short('p')
                 .long("depth")
                 .default_value("1")
-                .help("Scrawler depth.")
+                .help("Scrawler depth."),
         )
         .arg(
             Arg::new("ext")
-            .short('x')
-            .long("ext")
-            .default_value("pdf,zip")
-            .help("Target file name extension.") 
+                .short('x')
+                .long("ext")
+                .default_value("pdf,zip")
+                .help("Target file name extension."),
         )
         .arg(
             Arg::new("dir")
-            .short('d')
-            .long("dir")
-            .default_value(&shellexpand::tilde("~/Downloads").into_owned())
-            .help("Target dir.") 
+                .short('d')
+                .long("dir")
+                .default_value(&shellexpand::tilde("~/Downloads").into_owned())
+                .help("Target dir."),
         )
         .arg(
             Arg::new("home_url")
-               .required(true)
-               .index(1)
-               .help("Home Url of the site.")
+                .required(true)
+                .index(1)
+                .help("Home Url of the site."),
         )
         .get_matches();
 
-        //TODO: how to make the target_dir available without unwrap and if let statment.
-        let target_dir = app_m.value_of("dir").unwrap();
-        //The shellexpand has already done by shell command.
-        //let target_dir = shellexpand::tilde(cfg_dir).into_owned();
-        std::fs::create_dir_all(&target_dir).unwrap();
-    
-        if let Some(depth) = app_m.value_of("depth") {
-            println!("Scrawler depth: {depth}");
+    //TODO: how to make the target_dir available without unwrap and if let statment.
+    let target_dir = app_m.value_of("dir").unwrap();
+    //The shellexpand has already done by shell command.
+    //let target_dir = shellexpand::tilde(cfg_dir).into_owned();
+    std::fs::create_dir_all(&target_dir).unwrap();
 
-            if let Some(home_url) = app_m.value_of("home_url"){
-                println!("Home Url: {home_url}");
-                match retrieve_urls(home_url).await{
-                    Err(why) => println!("Failed to retrieve urls: {why}"),
-                    Ok(urls) => { 
-                        if app_m.is_present("explore") {
-                            let mut exts = std::collections::HashSet::new();
-                            for url in &urls {
-                                println!("{url}");
-                                if let Some(ext) = extract_extension(url) {
-                                    exts.insert(ext);
-                                }
+    if let Some(depth) = app_m.value_of("depth") {
+        println!("Scrawler depth: {depth}");
+
+        if let Some(home_url) = app_m.value_of("home_url") {
+            println!("Home Url: {home_url}");
+            match retrieve_urls(home_url).await {
+                Err(why) => println!("Failed to retrieve urls: {why}"),
+                Ok(urls) => {
+                    if app_m.is_present("explore") {
+                        let mut exts = std::collections::HashSet::new();
+                        for url in &urls {
+                            println!("{url}");
+                            if let Some(ext) = extract_extension(url) {
+                                exts.insert(ext);
                             }
-                            let v = Vec::from_iter(exts);
-                            println!("File Extensions: [{}]", v.join(", "))
-                        } else {
-                            if let Some(exts) = app_m.value_of("ext") {
-                                let exts: Vec<&str> = exts.split_terminator(",").collect();
-                                let urls = urls.into_iter().filter(|x|{ 
-                                    exts.iter().any(|e| x.ends_with(e))
-                                 }).collect::<Vec<String>>();
+                        }
+                        let v = Vec::from_iter(exts);
+                        println!("File Extensions: [{}]", v.join(", "))
+                    } else if let Some(exts) = app_m.value_of("ext") {
+                        let exts: Vec<&str> = exts.split_terminator(',').collect();
+                        let urls = urls
+                            .into_iter()
+                            .filter(|x| exts.iter().any(|e| x.ends_with(e)))
+                            .collect::<Vec<String>>();
 
-                                let urls = compose_absolute_urls(home_url, urls);
-                                for url in &urls {
-                                    if let Err(why) = download(url, target_dir).await {
-                                            println!("Download error: {why}");
-                                    }
-                                } 
+                        let urls = compose_absolute_urls(home_url, urls);
+                        for url in &urls {
+                            if let Err(why) = download(url, target_dir).await {
+                                println!("Download error: {why}");
                             }
                         }
                     }
                 }
-
             }
         }
+    }
 }
-
